322f0e360c2801d7773efadc79805069c1df74ef
soniagkalliath <soniageorge111@gmail.com>
2026-02-11 11:00:20 +0530

    COMPLETE: crud -design

---
+++ script.js
@@ -0,0 +1,19 @@
+let users = []
+
+//get  form & tbody
+
+const form = document.getElementById('userForm')
+const tableBody = document.getElementById('tableBody')
+
+//add entry to users
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+    // get name & email
+    const name = document.getElementById('name').value
+    const email = document.getElementById('email').value
+    //add name & email to users
+    users.push({name,email})
+    form.reset()
+    console.log(users);
+    
+})
