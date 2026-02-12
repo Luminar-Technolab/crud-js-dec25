58941131f7338f98150c6831c2d67cdaf33e6d6d
soniagkalliath <soniageorge111@gmail.com>
2026-02-12 09:52:50 +0530

    COMPLETE: crud app

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let users = []
 
 const form = document.getElementById('userForm')
 const tableBody = document.getElementById('tableBody')
+const editIndexInput = document.getElementById('editIndex')
 
 //add entry to users
 form.addEventListener("submit",(e)=>{
@@ -11,9 +12,47 @@ form.addEventListener("submit",(e)=>{
     // get name & email
     const name = document.getElementById('name').value
     const email = document.getElementById('email').value
-    //add name & email to users
-    users.push({name,email})
+    if(editIndexInput.value==""){
+        //add name & email to users
+        users.push({name,email})
+    }else{
+        //edit user
+        users[editIndexInput.value] = {name,email}
+        editIndexInput.value = ""
+    }
     form.reset()
     console.log(users);
-    
+    displayUsers()
 })
+//users should be listed inside table body
+const displayUsers = ()=>{
+    tableBody.innerHTML = ""
+    users.forEach((user,index)=>{
+        tableBody.innerHTML += `
+        <tr>
+            <td>${index+1}</td>
+            <td>${user.name}</td>
+            <td>${user.email}</td>
+            <td>
+                <div class="d-flex">
+                    <button onclick="editUser(${index})" class="btn btn-warning">edit</button>
+                    <button onclick="deleteUser(${index})" class="btn btn-danger ms-3">Delete</button>
+                </div>
+            </td>
+        </tr>`
+    })
+}
+
+//used to edit particular user
+const editUser = (index)=>{
+   document.getElementById('name').value =  users[index].name
+   document.getElementById('email').value = users[index].email
+   editIndexInput.value = index
+}
+
+const deleteUser = (index)=>{
+    if(confirm('Are you sure, do you want to delete the data?')){
+        users.splice(index,1)
+        displayUsers()
+    }
+}
